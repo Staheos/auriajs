f9c753b7d0eff21e7728ae5d649aa2bad454fbf8
Staheos <staslewy@gmail.com>
2025-10-18 11:36:58 +0200

    Added function to switch to local endpoint.

--- src/API.ts
+++ src/API.ts
@@ -12,44 +12,50 @@ const UNVERIFIED_API_ENDPOINT = axios.create({
   headers: { 'Content-Type': 'application/json' },
 });
 
-const LOCAL_API_ENDPOINT = axios.create({
-  baseURL: 'https://localhost:7190',
-  headers: { 'Content-Type': 'application/json' },
-});
-
-const LOCAL_UNVERIFIED_API_ENDPOINT = axios.create({
-  baseURL: 'http://localhost:7190',
-  headers: { 'Content-Type': 'application/json' },
-});
+// @desc Current API endpoint used for requests.
+let API_ENDPOINT = DEFAULT_API_ENDPOINT;
 
 /**
  * @desc
  *  Select the API endpoint used to make requests to the node.
  */
-let API_ENDPOINT = DEFAULT_API_ENDPOINT;
-export function SelectAPIEndpoint(isLocal: boolean, isUnverified: boolean): void {
-  if (isLocal) {
-    API_ENDPOINT = isUnverified ? LOCAL_UNVERIFIED_API_ENDPOINT : LOCAL_API_ENDPOINT;
+export function SelectAPIEndpoint(isUnverified: boolean): void {
+  API_ENDPOINT = isUnverified ? UNVERIFIED_API_ENDPOINT : DEFAULT_API_ENDPOINT;
+}
+
+/**
+ * @desc
+ *  Select local API endpoint with given address.
+ */
+export function SelectLocalAPIEndpoint(isUnverified: boolean, address: string): void {
+  if (isUnverified) {
+    API_ENDPOINT = axios.create({
+      baseURL: `http://${ address }:7190`,
+      headers: { 'Content-Type': 'application/json' },
+    });
   } else {
-    API_ENDPOINT = isUnverified ? UNVERIFIED_API_ENDPOINT : DEFAULT_API_ENDPOINT;
+    API_ENDPOINT = axios.create({
+      baseURL: `https://${ address }:7190`,
+      headers: { 'Content-Type': 'application/json' },
+    });
   }
 }
 
 /**
-   * @desc
-   *  Submit a signed transaction to the node via HTTP POST.
-   */
+ * @desc
+ *  Submit a signed transaction to the node via HTTP POST.
+ */
 export async function SubmitTransaction(tr: SignedTransaction): Promise<string> {
   const response = await API_ENDPOINT.post<string>('/st', tr.Serialize());
   return response.data;
 }
 
 /**
-   * @desc
-   *  Fetch the balance for the given account address. Returns a number on success or an error string on failure.
-   */
+ * @desc
+ *  Fetch the balance for the given account address. Returns a number on success or an error string on failure.
+ */
 export async function FetchBalance(accountAddress: string): Promise<number | string> {
-  const response = await API_ENDPOINT.get<string>(`/balance/${accountAddress}`);
+  const response = await API_ENDPOINT.get<string>(`/balance/${ accountAddress }`);
   const text = response.data;
   if (!(response.status === 200) || parseInt(text, 10) === Number.NaN) {
     return text;
