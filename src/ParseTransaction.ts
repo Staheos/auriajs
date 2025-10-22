d7c59a251f10bfe43248842b492a90e34712dc0b
Staheos <staslewy@gmail.com>
2025-10-22 20:51:53 +0200

    Fixed check conditions.

--- src/ParseTransaction.ts
+++ src/ParseTransaction.ts
@@ -5,19 +5,27 @@ import { SignedTransaction } from "./SignedTransaction.js";
 
 
 export function CheckTransactionType(v: any): TransactionType | undefined {
-  if ("amount" in v && typeof v.amount !== "number") return undefined;
-  if ("fees" in v && typeof v.fees !== "number") return undefined;
-  if ("recipient" in v && typeof v.recipient !== "string") return undefined;
-  if ("sender" in v && typeof v.sender !== "string") return undefined;
-  return v as TransactionType;
+  if (!"amount" in v || typeof v.amount !== "number")
+        return undefined;
+    if (!"fees" in v || typeof v.fees !== "number")
+        return undefined;
+    if (!"recipient" in v || typeof v.recipient !== "string")
+        return undefined;
+    if (!"sender" in v || typeof v.sender !== "string")
+        return undefined;
+    return v as TransactionType;
 }
 
 export function CheckSignedTransactionType(v: any): SignedTransactionType | undefined {
-  if ("transaction" in v && typeof v.amount !== "object") return undefined;
-  if ("hash" in v && typeof v.fees !== "string") return undefined;
-  if ("pubkey" in v && typeof v.recipient !== "string") return undefined;
-  if ("signature" in v && typeof v.sender !== "string") return undefined;
-  return v as SignedTransactionType;
+  if (! ("transaction" in v) || typeof v.transaction !== "object")
+        return undefined;
+    if (! ("hash" in v) || typeof v.hash !== "string")
+        return undefined;
+    if (! ("pubkey" in v) || typeof v.pubkey !== "string")
+        return undefined;
+    if (! ("signature" in v) || typeof v.signature !== "string")
+        return undefined;
+    return v as SignedTransactionType;
 }
 
 export function ParseTransaction(v: object): Transaction | SignedTransaction | undefined {
